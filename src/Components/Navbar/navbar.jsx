844b45cd3dac6ba06684071c2174f18680155aeb
RomiScript <romina.martinez496@gmail.com>
2025-12-02 22:45:11 -0300

    creando menú hamburguesa

--- src/Components/Navbar/navbar.jsx
+++ src/Components/Navbar/navbar.jsx
@@ -1,21 +1,37 @@
-import './navbar.css';
-import logo1 from '../../assets/images/logo1.png';
+import { useState } from "react";
+import "./navbar.css";
+import logo1 from "../../assets/images/logo1.png";
 
 function Navbar() {
+  const [open, setOpen] = useState(false);
+
   return (
-    <nav className='container'>
-      <img src={logo1} alt="Logo Club de Leones Junín-Autismo" width='250' />
-      <ul>
-        <li><a href="#inicio">Inicio</a></li>
-        <li><a href="#nosotros">Nosotros</a></li>
-        <li><a href="#actividades">Actividades</a></li>
-        <li><a href="#proyectos">Proyectos</a></li>
-      
-        <li><a href="#contacto" className='btn'>Contacto</a></li>
-      </ul>
+    <nav className="navbar">
+      <div className="nav-container">
+        <img
+          src={logo1}
+          alt="Logo Club de Leones Junín-Autismo"
+          className="nav-logo"
+        />
+
+        {/* ICONO HAMBURGUESA */}
+        <div className="hamburger" onClick={() => setOpen(!open)}>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+
+        <ul className={open ? "nav-links open" : "nav-links"}>
+          <li><a href="#inicio">Inicio</a></li>
+          <li><a href="#nosotros">Nosotros</a></li>
+          <li><a href="#actividades">Actividades</a></li>
+          <li><a href="#proyectos">Proyectos</a></li>
+          <li><a href="#contacto" className="btn-nav">Contacto</a></li>
+        </ul>
+      </div>
     </nav>
-    
   );
 }
 
 export default Navbar;
+
