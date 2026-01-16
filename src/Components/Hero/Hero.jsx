aa76e509af825cad178b2ead9f586d99042d4633
RomiScript <romina.martinez496@gmail.com>
2026-01-16 13:50:37 -0300

    hero arreglo

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -6,8 +6,8 @@ function Hero() {
   return (
     <div className='hero container'>
       <div className="hero-text">
-        <h1>Club de Leones Junín-Autismo presenta:</h1>
-        <h2>Autismo en movimiento</h2>
+        <h2>Club de Leones Junín-Autismo presenta:</h2>
+        <h1>Autismo en movimiento</h1>
         <h3>🏃🏽‍♀️‍➡️🚶🏽‍➡️👨🏽‍🦽‍➡️</h3>
         <p>La segunda edición de la carrera inclusiva está llegando. Preparate para correr, caminar, trotar o desplazarte en este evento de inclusión en comunidad</p>
        <a 
