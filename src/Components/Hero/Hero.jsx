012234d7f1a9ff2af48a28482e8f3cb438184dff
RomiScript <romina.martinez496@gmail.com>
2026-01-16 12:45:13 -0300

    Cambio hero section

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,20 +1,21 @@
 import './Hero.css'
-import Icon from '../../assets/images/icon.png'
+import Icon from '../../assets/images/icon2.png'
 
 
 function Hero() {
   return (
     <div className='hero container'>
       <div className="hero-text">
-        <h1>Club de Leones Junín-Autismo</h1>
-        <p>Incluyendo, sirviendo, creciendo.</p>
+        <h1>Autismo en movimiento</h1>
+        <h1>🏃🏽‍♀️‍➡️🚶🏽‍➡️👨🏽‍🦽‍➡️</h1>
+        <p>La segunda edición de la carrera inclusiva está llegando. Preparate para correr, caminar, trotar o desplazarte en este evento de inclusión en comunidad</p>
        <a 
   className="btn" 
   href="https://link.mercadopago.com.ar/juninautismo" 
   target="_blank" 
   rel="noopener noreferrer"
 >
-  Donar <img src={Icon} alt="" />
+  ¡Quiero participar! <img src={Icon} alt="" />
 </a>
       </div>
     </div>
