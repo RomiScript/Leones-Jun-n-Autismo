c8b169e78bbc3b21b9ec67e3db2fb7e4b8acc4dc
RomiScript <romina.martinez496@gmail.com>
2026-02-20 19:17:47 -0300

    aver

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Nosotros from './assets/images/viamonte.jpg';
 import Actividades from './assets/images/actividades.jpg'
 import Carrera from './assets/images/aem.JPG'
 
-import FloatingChat from '../src/FloatingChat';
+
 import IgIcon from './assets/images/ig-icon.avif'
 import FbIcon from './assets/images/facebook-icon.png'
 import Reme1 from './assets/images/reme1.jpeg';
@@ -19,7 +19,7 @@ function App() {
       <Navbar />
       <WhatsAppButton />
       
-      <FloatingChat />
+      
 
 
 
